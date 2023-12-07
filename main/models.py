62fcd3c0ee12c91ce827ea2e432281e42114775c
Asif-GD <asif.coach@gmail.com>
2023-12-08 02:49:58 +0530

    created the data model for our book table - BookList - added three books from the harry potter series as test data

--- main/models.py
+++ main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+# Book Model
+"""
+_________________________________________________
+| BookList                                      |
+-------------------------------------------------
+| book_title                    TEXT            |
+| book_isbn                     INTEGER         |
+| book_author                   TEXT            |
+| book_year_of_publication      INTEGER         |
+| book_genre                    TEXT            |
+| book_availability             INTEGER (1/0)   |
+-------------------------------------------------
+"""
+
+
+class BookList(models.Model):
+    # basic information
+    book_title = models.CharField(max_length=300)
+    book_isbn = models.IntegerField()
+    book_author = models.CharField(max_length=100)
+    book_year_of_publication = models.IntegerField()
+    book_genre = models.CharField(max_length=300)
+    # stock checking
+    book_availability = models.BooleanField(default=True)
+    # soft deletion
+    book_delete_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.book_title
