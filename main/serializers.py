5409694262490f436ca470682a0190e436a3c229
Asif-GD <asif.coach@gmail.com>
2023-12-10 21:35:23 +0530

    setup exception handling, error statuses for add_book api

--- main/serializers.py
+++ main/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
+from rest_framework.fields import CharField, IntegerField, BooleanField
 
 from .models import BookList
 
 
 class BookListSerializer(serializers.ModelSerializer):
+
+    book_title = CharField(required=True)
+    book_isbn_13 = IntegerField(required=True)
+    book_author = CharField(required=True)
+    book_year_of_publication = IntegerField(required=True)
+    book_genre = CharField(required=True)
+    # stock checking
+    book_availability = BooleanField(default=True)
+    # soft deletion
+    book_delete_status = BooleanField(default=False)
+
     class Meta:
         model = BookList
         fields = "__all__"
--- main/views.py
+++ main/views.py
@@ -1,4 +1,9 @@
+from json import JSONDecodeError
+
+from django.http import JsonResponse
+from rest_framework import status
 from rest_framework.decorators import api_view
+# from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 
 from .models import BookList
@@ -15,7 +20,13 @@ def get_all_books(request):
 
 @api_view(["POST"])
 def add_book(request):
-    serializer = BookListSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+    try:
+        # data = JSONParser().parse(request)
+        serializer = BookListSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except JSONDecodeError:
+        return JsonResponse({"result": "error", "message": "Json decoding error"}, status=400)
