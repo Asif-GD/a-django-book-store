18467eba19271216d778704a0e2a6625aa4c4f16
Asif-GD <asif.coach@gmail.com>
2023-12-11 04:54:07 +0530

    added 'django_extensions' & 'django_filters' to INSTALLED_APPS

--- book_store/settings.py
+++ book_store/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
+    'django_filters',
     'main.apps.MainConfig',
     'rest_framework',
 ]
