fbb51af98459e4d5444378db749c8b219701f793
Asif-GD <asif.coach@gmail.com>
2023-12-10 18:04:50 +0530

    implemented routers for automatic URL routing.

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.get_all_books, name="landing_page"),
+    path("get_all_books/", views.get_all_books, name="view_all_books"),
     path("add_book/", views.add_book, name="add_book"),
 ]
