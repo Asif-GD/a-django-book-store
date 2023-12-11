074bdf6bf9b721bbde430a86633f5e0628171ed6
Asif-GD <asif.coach@gmail.com>
2023-12-11 22:39:02 +0530

    reworked on the BookList model
    - inherits TitleSlugDescriptionModel
    - added a method on_delete() to perform soft deletion

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django_extensions.db.models import TitleSlugDescriptionModel
 
 # Create your models here.
 # Book Model
@@ -6,19 +7,33 @@ from django.db import models
 _________________________________________________
 | BookList                                      |
 -------------------------------------------------
-| book_title                    TEXT            |
+| title                         TEXT            |
 | book_isbn                     INTEGER         |
 | book_author                   TEXT            |
 | book_year_of_publication      INTEGER         |
 | book_genre                    TEXT            |
-| book_availability             INTEGER (1/0)   |
+| book_availability             BOOLEAN         |
+| book_delete_status            BOOLEAN         |
 -------------------------------------------------
 """
 
 
-class BookList(models.Model):
+class BookList(TitleSlugDescriptionModel,
+               models.Model):
+    """
+    Stores a single book entry for our book store
+    """
+
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.title
+
     # basic information
-    book_title = models.CharField(max_length=300)
+    title = models.CharField(max_length=300)
     book_isbn_13 = models.IntegerField(unique=True)
     book_author = models.CharField(max_length=100)
     book_year_of_publication = models.IntegerField()
@@ -28,5 +43,7 @@ class BookList(models.Model):
     # soft deletion
     book_delete_status = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.book_title
+    def on_delete(self):
+        # to perform soft deletion of a book
+        self.book_delete_status = True
+        self.save()
