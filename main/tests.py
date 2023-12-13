c19727bc4e25d9b59d63fdfa54009864b86d9ef1
Asif-GD <asif.coach@gmail.com>
2023-12-13 16:55:50 +0530

    setup test cases for RestoreSoftDeletedBookViewSet

--- main/tests.py
+++ main/tests.py
@@ -23,7 +23,11 @@ class BookListTestCase(APITestCase):
                                 book_author="J.K. Rowling", book_year_of_publication=2014, book_genre="fantasy")
         BookList.objects.create(title="Harry Potter and the Goblet of Fire", book_isbn_13=9781408855683,
                                 book_author="J.K. Rowling", book_year_of_publication=2014, book_genre="fantasy")
+
         self.books = BookList.objects.all()
+        self.soft_deleted_books = BookList.objects.filter(book_delete_status=True)
+        self.restored_books = BookList.objects.filter(book_delete_status=False)
+
         self.user = User.objects.create_user(
             username='testuser1',
             password='this_is_a_test',
@@ -39,12 +43,13 @@ class BookListTestCase(APITestCase):
         self.data = {
             "data": {
                 "type": "BookList",
+                "id": "5",
                 "attributes": {
                     "title": "Atomic Habits",
                     "book_isbn_13": 9781847941831,
                     "book_author": "James Clear",
                     "book_year_of_publication": 2018,
-                    "book_genre": "self-help",
+                    "book_genre": "self help",
                     "book_availability": True,
                     "book_delete_status": False
                 }
@@ -64,7 +69,6 @@ class BookListTestCase(APITestCase):
     def test_get_one_book(self):
         """
         test BookListViewSet retrieve method
-        :return: Ok
         """
         for book in self.books:
             response = self.client.get(path=self.url + f"{book.id}/")
@@ -173,14 +177,101 @@ class BookListTestCase(APITestCase):
         response = self.client.post(path=self.url, data=self.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_soft_delete_book(self):
-    #     """
-    #     test BookListViewSet delete method
-    #     :return:
-    #     """
-    #     book = self.data["data"]["attributes"]
-    #     self.assertEqual(book["book_delete_status"], False)
-    #     book["book_delete_status"] = True
-    #     response = self.client.patch(path=self.url + f"{self.books[0].id}/", data=self.data)
-    #     self.assertEqual(book["book_delete_status"], True)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_soft_delete_book(self):
+        """
+        test BookListViewSet delete method
+        """
+        book = self.books.filter(title="Harry Potter and the Chamber of Secrets")
+        book_id = book[0].id
+        response = self.client.delete(path=self.url + f"{book_id}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.soft_deleted_books.count(), 1)
+
+
+class RestoreSoftDeletedBookViewSet(APITestCase):
+    """
+    Test suite for RestoreSoftDeletedBookViewSet
+    """
+
+    def setUp(self):
+        BookList.objects.create(title="Harry Potter and the Philosopher\'s Stone", book_isbn_13=9781408855652,
+                                book_author="J.K. Rowling", book_year_of_publication=2014, book_genre="fantasy",
+                                book_delete_status=False, )
+        BookList.objects.create(title="Harry Potter and the Chamber of Secrets", book_isbn_13=9781408855669,
+                                book_author="J.K. Rowling", book_year_of_publication=2014, book_genre="fantasy",
+                                book_delete_status=True, )
+        BookList.objects.create(title="Harry Potter and the Prisoner of Azkaban", book_isbn_13=9781408855676,
+                                book_author="J.K. Rowling", book_year_of_publication=2014, book_genre="fantasy",
+                                book_delete_status=False, )
+        BookList.objects.create(title="Harry Potter and the Goblet of Fire", book_isbn_13=9781408855683,
+                                book_author="J.K. Rowling", book_year_of_publication=2014, book_genre="fantasy",
+                                book_delete_status=True, )
+
+        self.books = BookList.objects.all()
+        self.soft_deleted_books = BookList.objects.filter(book_delete_status=True)
+        self.restored_books = BookList.objects.filter(book_delete_status=False)
+
+        self.user = User.objects.create_user(
+            username='testuser1',
+            password='this_is_a_test',
+            email='testuser1@test.com'
+        )
+
+        # app uses token authentication
+        self.token = Token.objects.get(user=self.user)
+        self.client = APIClient()
+        # we pass the token in all calls to the API
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.data = {
+            "data": {
+                "type": "BookList",
+                "id": "2",
+                "attributes": {
+                    "title": "Harry Potter and the Chamber of Secrets",
+                    "book_isbn_13": 9781408855669,
+                    "book_author": "J.K. Rowling",
+                    "book_year_of_publication": 2014,
+                    "book_genre": "fantasy",
+                    "book_availability": True,
+                    "book_delete_status": True
+                }
+            }
+        }
+
+        self.url = "/restore-deleted-book/"
+
+    def test_get_all_soft_deleted_books(self):
+        """
+        test RestoreSoftDeletedBookViewSet list method
+        """
+        self.assertEqual(self.soft_deleted_books.count(), 2)
+        response = self.client.get(path=self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_one_book_from_soft_deleted_books(self):
+        """
+        test RestoreSoftDeletedBookViewSet retrieve method on soft deleted books
+        """
+        for book in self.soft_deleted_books:
+            response = self.client.get(path=self.url + f"{book.id}/")
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_one_book_restored_books(self):
+        """
+        test RestoreSoftDeletedBookViewSet retrieve method on restored books
+        """
+        for book in self.restored_books:
+            response = self.client.get(path=self.url + f"{book.id}/")
+            # because we implemented to get only soft deleted books
+            self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_restore_book(self):
+        """
+        test RestoreSoftDeletedBookViewSet partial update method
+        """
+        book = self.soft_deleted_books.filter(title="Harry Potter and the Chamber of Secrets")
+        book_id = book[0].id
+        response = self.client.patch(path=self.url + f"{book_id}/", data=self.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.restored_books.count(), 3)
